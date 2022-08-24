653ff5a61f31a1092474b719e69a6d71b90b3949
Alan Hape <ahape@brightmetrics.com>
2022-08-24 14:32:10 -0700

    Initial commit

---
+++ LibraryA/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace LibraryA
+{
+    public class Class1
+    {
+        private readonly string version;
+        public string DependsOn => "Library A depends on SharedDependency version " + version;
+        public Class1()
+        {
+            var inst = new SharedDependency.SharedThing();
+            version = inst.Version;
+        }
+    }
+}
---
+++ SharedDependency/SharedThing.cs
@@ -0,0 +1,11 @@
+﻿namespace SharedDependency
+{
+    public class SharedThing
+    {
+        public string Version { get; set; }
+        public SharedThing()
+        {
+            Version = "1.0.0.1";
+        }
+    }
+}
