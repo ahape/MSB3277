459e23f8d85cd84a31742b48efd5bd56756fea12
Alan Hape <ahape@brightmetrics.com>
2022-08-24 14:34:11 -0700

    Fix submodule thing

---
+++ ExampleProgram/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ExampleProgram
+{
+    static class Program
+    {
+        static void Main()
+        {
+            System.Console.WriteLine("Direct dependency on SharedDependency version " + new SharedDependency.SharedThing().Version);
+            System.Console.WriteLine(new LibraryA.Class1().DependsOn);
+            System.Console.WriteLine(new LibraryB.Class1().DependsOn);
+            System.Console.ReadLine();
+        }
+    }
+}
